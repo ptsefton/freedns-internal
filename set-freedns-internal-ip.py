b41a5195510655741b355c110226cb208f1cc4e9
Oliver Caldwell <ollie@qubit.com>
2015-11-26 14:35:32 +0000

    Make file paths relative

--- set-freedns-internal-ip.py
+++ set-freedns-internal-ip.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
+
 import os
 import socket
 import json
 import socket
-import os
 from urllib.request import urlopen
 
-secret = json.load(open(".freedns_secret","r"))['secret']
-url = "https://freedns.afraid.org/dynamic/update.php?%s&address=%s" 
+dir = os.path.dirname(__file__)
+secret_path = os.path.join(dir, '.freedns_secret')
+ip_path = os.path.join(dir, '.freedns_secret')
 
+secret = json.load(open(secret_path, 'r'))['secret']
+url = 'https://freedns.afraid.org/dynamic/update.php?%s&address=%s'
 
-
-
-
-f = open(".ip", 'a').close()
-f = open(".ip",'r')
+f = open(ip_path, 'a').close()
+f = open(ip_path, 'r')
 oldip = f.read()
 f.close()
 
-
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("gmail.com",80))
+s.connect(('gmail.com', 80))
 ip = s.getsockname()[0]
 
-open(".ip","w").write(ip)
+open(ip_path, 'w').write(ip)
 if ip != oldip:
      new_url = url % (secret, ip)
      urlopen(new_url).read()
